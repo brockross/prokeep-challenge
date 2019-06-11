28dd797cf05e5939a44daf5a5a507477b90897c2
Brock Ross <m.brock.ross@gmail.com>
2019-06-11 11:44:14 -0600

    add input handler and attach to form fields

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -24,13 +24,22 @@ class Form extends React.Component {
     }
   }
 
+  handleInput(e) {
+    let text = e.target.value;
+    let field = e.target.name;
+
+    this.setState({
+      [field] : [text]
+    })
+  }
+
   render() {
     return (
       <FlexForm>
         <Label>Email:</Label>
-        <input type="text"></input>
+        <input type="text" name="email" onChange={(e) => this.handleInput(e)}></input>
         <Label>Password:</Label>
-        <input type="text"></input>
+        <input type="text" name="password" onChange={(e) => this.handleInput(e)}></input>
         <input type="submit"></input>
       </FlexForm>
     )
