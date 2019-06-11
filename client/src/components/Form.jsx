2e0a4f09e9f1cc6bc6511d286b7494118cc2515a
Brock Ross <m.brock.ross@gmail.com>
2019-06-11 12:11:49 -0600

    disable submit based on email/pw validation

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -18,6 +18,7 @@ const InvalidMessage = styled.p`
   visibility: ${props => props.isValid ? 'hidden' : 'visible'};
 `
 
+
 class Form extends React.Component {
   constructor(props) {
     super(props)
@@ -43,18 +44,25 @@ class Form extends React.Component {
     return this.state.password.length;
   }
 
+  handleSubmit() {
+
+  }
+
   render() {
     const {email, password} = this.state;
 
     return (
       <FlexForm>
+
         <Label>Email:</Label>
         <input type="text" name="email" onChange={(e) => this.handleInput(e)}></input>
         <InvalidMessage isValid={validate(email.toString())}>Please enter a valid email.</InvalidMessage>
+
         <Label>Password:</Label>
         <input type="text" name="password" onChange={(e) => this.handleInput(e)}></input>
         <InvalidMessage isValid={!!this.validatePW()}>Please enter a password.</InvalidMessage>
-        <input type="submit"></input>
+
+        <input type="submit" disabled={!(validate(email.toString()) && this.validatePW() )}></input>
       </FlexForm>
     )
   }
